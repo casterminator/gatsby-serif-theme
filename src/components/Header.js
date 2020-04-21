63c81649adc900aeb4ead663a170cd48c717a988
casterminator <44626745+casterminator@users.noreply.github.com>
2020-04-21 15:49:16 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 import Menu from './Menu';
 import Hamburger from './Hamburger';
 import logo from '../images/learnovate.png';
-import logoMobile from '../images/learnovate.png';
+import logoMobile from '../images/learnovate-Mobile.png';
 import MenuMobile from './MenuMobile';
 
 class Header extends React.Component {
